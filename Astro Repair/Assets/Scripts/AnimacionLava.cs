6bfcb2486d76e2929e8885ec1011a77294065cd6
Salax-18 <katheduque08@gmail.com>
2025-05-06 12:55:14 -0500

    Río lava
    Se agregaron algunos ríos de lava utilizando ProBuilder

--- Astro Repair/Assets/Scripts/AnimacionLava.cs
+++ Astro Repair/Assets/Scripts/AnimacionLava.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class AnimacionLava : MonoBehaviour
 {
-    public float scrollX = 0.1f;
-    public float scrollY = 0.1f;
+    public float velocidadX = 0.2f;
+    public float velocidadY = 0.1f;
+    public int indiceMaterialLava = 0; 
 
     private Renderer rend;
+    private Material lavaMaterial;
 
     void Start()
     {
         rend = GetComponent<Renderer>();
+
+       
+        lavaMaterial = rend.materials[indiceMaterialLava];
     }
 
     void Update()
     {
-        float offsetX = Time.time * scrollX;
-        float offsetY = Time.time * scrollY;
-        rend.material.mainTextureOffset = new Vector2(offsetX, offsetY);
+        float offsetX = Time.time * velocidadX;
+        float offsetY = Time.time * velocidadY;
+
+        lavaMaterial.mainTextureOffset = new Vector2(offsetX, offsetY);
     }
 }
